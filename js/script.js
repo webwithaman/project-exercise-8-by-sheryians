4811362631a3552ab31564c6fc57e889798d4c21
aman <webwithaman06@gmail.com>
2025-04-15 18:35:08 +0530

    refactor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
+// Get Elements
 let addBtn = document.querySelector("#add-btn");
-let inputElemet = document.querySelector("input");
+let inputField = document.querySelector("input");
 let itemsContainer = document.querySelector(".items-container");
 
+// Adding Click Event on Add Button to Add Items
 addBtn.onclick = () => {
+  // Create li Element
   let li = document.createElement("li");
-  if (inputElemet.value) {
+
+  // If Input Field is Non-Empty then Append li to Items Container
+  if (inputField.value) {
     if (!itemsContainer.firstElementChild) itemsContainer.innerHTML = "";
-    li.innerHTML = inputElemet.value;
-    inputElemet.value = "";
+    li.innerHTML = inputField.value;
+    inputField.value = "";
     itemsContainer.appendChild(li);
   }
 };
